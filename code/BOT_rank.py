27754b8506755078bc267dd8ffdfc42cfd96920a
FRNessar <61246905+FRNessar@users.noreply.github.com>
2020-03-28 17:35:48 +0100

    Update BOT_rank.py

--- code/BOT_rank.py
+++ code/BOT_rank.py
@@ -74,7 +74,7 @@ def prog(ts3conn, jeux, groupe):
 with ts3.query.TS3Connection("localhost") as ts3conn:
 	ts3conn.login(
 		client_login_name="***REMOVED***",
-		client_login_password="***REMOVED***"
+		client_login_password=""
 	)
 	ts3conn.use(sid=1)
 	ts3conn.clientupdate(client_nickname="Bot grade")
